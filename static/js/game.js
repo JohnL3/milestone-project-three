33b070a7a0b573044e635c707b585812473a4adb
John L <johnlonggately@gmail.com>
2018-08-30 15:10:45 +0000

    added code to game javascript to post answers to questions to answer route

--- static/js/game.js
+++ static/js/game.js
@@ -39,6 +39,89 @@ $('.sqr').click(function(e){
   }
 });
 
+
+// post answer to question to server 
+$('#ans-button').click(()=>{
+  
+  let _id = $('.surround').attr('id');
+  
+  if(Object.getOwnPropertyNames(previousData).length > 2) {
+    $('input:checkbox:checked').each(function() {
+      answers.push($(this).val());
+    });
+    
+    let answerData = {
+      questionId: _id,
+      answer: answers
+    };
+    if(answers.length > 0){
+    console.log('answerData',answerData);
+    postAnswers(answerData);
+    } else {
+      $('.error-msg').css('display','flex');
+      setTimeout(function(){
+        $('.error-msg').css('display','none');
+      },3000);
+    }
+  } else {
+    let answers = $('.ans-inp').val().split(/[\\,]\s|[\s\\,]/);
+    if(answers != '') {
+      let answerData = {
+        'questionId': _id,
+        'answer': answers
+      };
+      
+      console.log('answerData',answerData);
+      postAnswers(answerData);
+    } else {
+      $('.error-msg').css('display','flex');
+      setTimeout(function(){
+        $('.error-msg').css('display','none');
+      },3000);
+    }
+  }
+});
+
+function postAnswers(data) {
+  let url = 'http://question-answer-johnl3-1.c9users.io/answer';
+
+  $.ajax({
+      type : 'POST',
+      url : url,
+      contentType: 'application/json;charset=UTF-8',
+      dataType: 'json',
+      data : JSON.stringify(data),
+        success: function(d){
+        console.log('answered data',d);
+        $('.q-a-outer').css('display','none');
+        $('#section-c').remove();
+         if('game-over' in d) {
+            setTimeout(function(){
+                if (d.msg[0].result === 'correct') {
+                  $('#'+d.msg[0].id).addClass('correct answered');
+                } else {
+                  $('#'+d.msg[0].id).addClass('wrong answered');
+                }
+                answers = [];
+                
+              },500);
+          } else {
+              setTimeout(function(){
+                //$('.q-a-outer').css('display','none');
+                //$('#section-c').remove();
+                if (d.msg[0].result === 'correct') {
+                  $('#'+d.msg[0].id).addClass('correct answered');
+                } else {
+                  $('#'+d.msg[0].id).addClass('wrong answered');
+                }
+                answers = [];
+              },500);
+          }
+        }
+    });
+}
+
+
 //Display question on page add required elements
 function createQA(data, id) {
 $('.surround').attr('id', id);
