073a6dab8ad75b7c67d329a3ae482e2b8b07af1b
John L <johnlonggately@gmail.com>
2018-11-11 19:02:38 +0000

    updated all questions

--- data/questions_answers.py
+++ data/questions_answers.py
@@ -1,41 +1,45 @@
 my_q_a = {
         '0': {
-            'type': 'python',
-            'question':'Name the four data types used in Python',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['Dictionary', 'List', 'Set', 'Tuple']
+            'type': 'javascript',
+            'question':'Which of the following methods will add one or more elements to the begenning of an array',
+            'description': 'Choose from the options provided.',
+            'choices': ['push()', 'shift()', 'unshift()', 'pop()'],
+            'answer': ['unshift()']
         },
         '1': {
-            'type': 'python',
-            'question': 'Which of the following is a data type used in Python',
-            'description': 'Choose all that apply.',
-            'choices': ['array', 'tuple', 'set', 'object', 'dictionary'],
-            'answer': ['dictionary', 'tuple', 'set']
+            'type': 'javascript',
+            'question': 'What dose the following print in console: console.log([1,2,3].pop())',
+            'description': 'Choose from the options provided.',
+            'choices': ['1,2', '3'],
+            'answer': ['3']
         },
         '2': {
             'type': 'javascript',
-            'question': 'Name three string methods.',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['charat', 'includes', 'indexof']
+            'question': 'Which method returns a shallow copy of a portion of an array into a new array.',
+            'description': 'Choose from the options provided.',
+            'choices': ['Slice', 'Splice'],
+            'answer': ['Slice']
         },
         '3': {
-            'type': 'python',
-            'question':'Name the four data types used in Python',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['Dictionary', 'List', 'Set', 'Tuple']
+            'type': 'javascript',
+            'question':'Which of the following statements is correct',
+            'description': 'Choose from the options provided.',
+            'choices': ['A set can only contain 3 elements', 'A set can only contain 3 unique elements', 'All elements in a set are unique'],
+            'answer': ['All elements in a set are unique']
         },
         '4': {
-            'type': 'python',
-            'question': 'Which of the following is a data type used in Python',
-            'description': 'Choose all that apply.',
-            'choices': ['array', 'tuple', 'set', 'object', 'dictionary'],
-            'answer': ['dictionary', 'tuple', 'set']
+            'type': 'javascript',
+            'question': 'The _____  method calls a function with a given this value and arguments provided individually',
+            'description': 'Choose the method to use.',
+            'choices': ['call()', 'apply()'],
+            'answer': ['call()']
         },
         '5': {
             'type': 'javascript',
-            'question': 'Name three string methods.',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['charat', 'includes', 'indexof']
+            'question': 'Whcih of the following would give an array containing the number 7.',
+            'description': 'Choose all that apply.',
+            'choices': ['Arry(7)', 'Arry.of(7)', 'Arry(7,5)'],
+            'answer': ['Arry.of(7)', 'Arry(7,5)']
         },
         '6': {
             'type': 'python',
@@ -44,23 +48,25 @@ my_q_a = {
             'answer': ['Dictionary', 'List', 'Set', 'Tuple']
         },
         '7': {
-            'type': 'python',
-            'question': 'Which of the following is a data type used in Python',
+            'type': 'javascript',
+            'question': 'Which of the following, if any are valid',
             'description': 'Choose all that apply.',
-            'choices': ['array', 'tuple', 'set', 'object', 'dictionary'],
-            'answer': ['dictionary', 'tuple', 'set']
+            'choices': ['let add = x,y => x+y;', 'let addTo = x => x+5;', 'let addTo = (x) => {return x+5;}', 'let addTo = (x) => { x+5;}'],
+            'answer': ['let addTo = x => x+5;', 'let addTo = (x) => {return x+5;}']
         },
         '8': {
             'type': 'javascript',
-            'question': 'Name three string methods.',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['charat', 'includes', 'indexof']
+            'question': 'Is the following Valid  let add = (x = 5, y) => x+y;.',
+            'description': 'Choose all that apply.',
+            'choices': ['Invalid as no return statement','Valid as its an arrow function','Invalid: Optional parameters are defined at the end of the parameter list', 'Invalid as no {} used'],
+            'answer': ['Invalid: Optional parameters are defined at the end of the parameter list']
         },
         '9': {
-            'type': 'python',
-            'question':'Name the four data types used in Python',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['Dictionary', 'List', 'Set', 'Tuple']
+            'type': 'javascript',
+            'question':'For fun what would the following show in console: let num=["10","20"]; +"10"+ ++num[0];',
+            'description': 'Choose from the options provided.',
+            'choices':['undefined','error message', '"1020"', 21, 20],
+            'answer': ["21"]
         },
         '10': {
             'type': 'python',
@@ -71,8 +77,9 @@ my_q_a = {
         },
         '11': {
             'type': 'javascript',
-            'question': 'Name three string methods.',
-            'description': 'Enter you answer below using a space or comma seperated list.',
-            'answer': ['charat', 'includes', 'indexof']
+            'question': 'Which of the following are javascript string methods.',
+            'description': 'Choose all that apply.',
+            'choices': ['reverse', 'replace', 'split', 'splice', 'slice'],
+            'answer': ['replace', 'split']
         },
 }
